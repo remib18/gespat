5169cea346a344ba03d1e26ff2009c23298ee8c9
Rémi Bernard <remib18@outlook.fr>
2022-01-06 02:08:48 +0100

    Safe patient supression

--- src/gespat/controllers/ConsultationController.java
+++ src/gespat/controllers/ConsultationController.java
@@ -15,85 +15,104 @@ import java.util.List;
 
 public class ConsultationController extends AbstractController<Consultation> {
 
-    /** Instance du PatientController */
-    private final PatientController patientCtrl;
-    private final DeviceController deviceCtrl;
+	/**
+	 * Instance du PatientController
+	 */
+	private final PatientController patientCtrl;
+	private final DeviceController deviceCtrl;
 
-    /**
-     * Crée un objet permettant la manipulation des consultations
-     * @param patientCtrl
-     * @throws ProcessingException en cas d'erreur lors du chargement des fichiers
-     */
-    public ConsultationController(PatientController patientCtrl, DeviceController deviceCtrl)
-            throws ProcessingException {
-        this.patientCtrl = patientCtrl;
-        this.deviceCtrl = deviceCtrl;
-        storeFile = "consultations.txt";
-        stateDataType = StateManager.DataType.Patient;
+	/**
+	 * Crée un objet permettant la manipulation des consultations
+	 *
+	 * @param patientCtrl
+	 * @throws ProcessingException en cas d'erreur lors du chargement des fichiers
+	 */
+	public ConsultationController(PatientController patientCtrl, DeviceController deviceCtrl)
+			throws ProcessingException {
+		this.patientCtrl = patientCtrl;
+		this.deviceCtrl = deviceCtrl;
+		storeFile = "consultations.txt";
+		stateDataType = StateManager.DataType.Patient;
 
-        load();
-    }
+		load();
+	}
 
-    /**
-     * Crée une nouvelle consultation
-     * @param patient
-     * @param doctorName
-     * @param consultedAt
-     * @param diagnosedPathologies
-     * @param requiredEquipment
-     * @param observations
-     * @throws ConflictingDataException
-     * @throws ProcessingException
-     */
-    public Consultation add(
-            Patient patient,
-            String doctorName,
-            LocalDate consultedAt,
-            String[] diagnosedPathologies,
-            Device requiredEquipment,
-            String observations
-    ) throws ConflictingDataException, ProcessingException {
-        Device device = requiredEquipment == null ? deviceCtrl.add(Device.STATES.UNDEFINED, null) : requiredEquipment;
-        return add(new Consultation(
-                StateManager.getState().getNextInsertionIndex(StateManager.DataType.Consultation),
-                patient,
-                doctorName,
-                consultedAt,
-                diagnosedPathologies,
-                device,
-                observations
-        ));
-    }
+	/**
+	 * Crée une nouvelle consultation
+	 *
+	 * @param patient
+	 * @param doctorName
+	 * @param consultedAt
+	 * @param diagnosedPathologies
+	 * @param requiredEquipment
+	 * @param observations
+	 * @throws ConflictingDataException
+	 * @throws ProcessingException
+	 */
+	public Consultation add(
+			Patient patient,
+			String doctorName,
+			LocalDate consultedAt,
+			String[] diagnosedPathologies,
+			Device requiredEquipment,
+			String observations
+	) throws ConflictingDataException, ProcessingException {
+		Device device = requiredEquipment == null ? deviceCtrl.add(Device.STATES.UNDEFINED, null) : requiredEquipment;
+		return add(new Consultation(
+				StateManager.getState().getNextInsertionIndex(StateManager.DataType.Consultation),
+				patient,
+				doctorName,
+				consultedAt,
+				diagnosedPathologies,
+				device,
+				observations
+		));
+	}
 
-    /**
-     * @return l'ensemble des pathologies enregistrées
-     */
-    public List<String> getAllPathologies() {
-        final List<String> pathologies = new ArrayList<>();
-        for (Consultation consultation : data) {
-            for (String pathology : consultation.getDiagnosedPathologies()) {
-                if (!pathologies.contains(pathology)) {
-                    pathologies.add(pathology);
-                }
-            }
-        }
-        return pathologies;
-    }
+	/**
+	 * @return l'ensemble des pathologies enregistrées
+	 */
+	public List<String> getAllPathologies() {
+		final List<String> pathologies = new ArrayList<>();
+		for (Consultation consultation : data) {
+			for (String pathology : consultation.getDiagnosedPathologies()) {
+				if (!pathologies.contains(pathology)) {
+					pathologies.add(pathology);
+				}
+			}
+		}
+		return pathologies;
+	}
 
-    @Override
-    protected Consultation makeObjectFromString(String[] object)
-            throws NotFoundException, NumberFormatException, ProcessingException {
-        String[] device = {object[5], object[6], object[7]};
-        List<String> pathologies = Regex.getMatches("[^|]+", object[4]);
-        //noinspection EmpryBranchInAlteration
-        return new Consultation(
-                Integer.parseInt(object[0]),
-                patientCtrl.get(Integer.parseInt(object[1])),
-                object[2],
-                LocalDate.parse(object[3]),
-                pathologies.toArray(new String[0]),
-                deviceCtrl.makeObjectFromString(device),
-                object[8]
-        );
-    }
+	public List<Consultation> getAll(Patient patient) {
+		List<Consultation> consultations = new ArrayList<>();
+		for (Consultation c : data) {
+			if (c.getPatient().equals(patient))
+				consultations.add(c);
+		}
+		return consultations;
+	}
+
+	void batchRemove(List<Consultation> consultations) throws NotFoundException, ProcessingException {
+		for (Consultation consultation : consultations) {
+			remove(consultation, true);
+		}
+	}
+
+	@Override
+	protected Consultation makeObjectFromString(String[] object)
+			throws NotFoundException, NumberFormatException, ProcessingException {
+		String[] device = {object[5], object[6], object[7]};
+		List<String> pathologies = Regex.getMatches("[^|]+", object[4]);
+		//noinspection EmpryBranchInAlteration
+		return new Consultation(
+				Integer.parseInt(object[0]),
+				patientCtrl.get(Integer.parseInt(object[1])),
+				object[2],
+				LocalDate.parse(object[3]),
+				pathologies.toArray(new String[0]),
+				deviceCtrl.makeObjectFromString(device),
+				object[8]
+		);
+	}
 }
--- src/gespat/controllers/PatientController.java
+++ src/gespat/controllers/PatientController.java
@@ -10,37 +10,55 @@ import java.time.LocalDate;
 
 public class PatientController extends AbstractController<Patient> {
 
-    /**
-     * Crée un objet permettant la manipulation des consultations
-     * @throws ProcessingException en cas d'erreur lors du chargement des fichiers
-     */
-    public PatientController() throws ProcessingException {
-        storeFile = "patients.txt";
-        stateDataType = StateManager.DataType.Patient;
+	private ConsultationController consultCtrl = null;
 
-        load();
-    }
+	/**
+	 * Crée un objet permettant la manipulation des consultations
+	 *
+	 * @throws ProcessingException en cas d'erreur lors du chargement des fichiers
+	 */
+	public PatientController() throws ProcessingException {
+		storeFile = "patients.txt";
+		stateDataType = StateManager.DataType.Patient;
 
-    public Patient add(String firstname, String lastname, int socialId, LocalDate birthAt) throws ConflictingDataException, ProcessingException {
-        return super.add(new Patient(
-                StateManager.getState().getNextInsertionIndex(StateManager.DataType.Patient),
-                firstname,
-                lastname,
-                socialId,
-                birthAt
-        ));
-    }
+		load();
+	}
 
-    @Override
-    protected Patient makeObjectFromString(String[] object)
-            throws NumberFormatException {
-        return new Patient(
-                Integer.parseInt(object[0]),
-                object[1],
-                object[2],
-                Integer.parseInt(object[3]),
-                LocalDate.parse(object[4])
-        );
-    }
+	public void setConsultCtrl(ConsultationController consultCtrl) {
+		this.consultCtrl = consultCtrl;
+	}
+
+	public Patient add(String firstname, String lastname, int socialId, LocalDate birthAt) throws ConflictingDataException, ProcessingException {
+		return super.add(new Patient(
+				StateManager.getState().getNextInsertionIndex(StateManager.DataType.Patient),
+				firstname,
+				lastname,
+				socialId,
+				birthAt
+		));
+	}
+
+	@Override
+	public void remove(int id, boolean confirmation) throws NotFoundException, ProcessingException {
+		remove(get(id), confirmation);
+	}
+
+	@Override
+	public void remove(Patient object, boolean confirmation) throws NotFoundException, ProcessingException {
+		consultCtrl.batchRemove(consultCtrl.getAll(object));
+		super.remove(object, confirmation);
+	}
+
+	@Override
+	protected Patient makeObjectFromString(String[] object)
+			throws NumberFormatException {
+		return new Patient(
+				Integer.parseInt(object[0]),
+				object[1],
+				object[2],
+				Integer.parseInt(object[3]),
+				LocalDate.parse(object[4])
+		);
+	}
 
 }
